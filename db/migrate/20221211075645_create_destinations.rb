8341ad0f3cc8d67841158afa4329886ed3cd56f2
Benaiah Wepundi <81746486+benaiah-ke@users.noreply.github.com>
2022-12-11 15:19:39 +0300

    add files

---
+++ db/migrate/20221211075645_create_destinations.rb
@@ -0,0 +1,12 @@
+class CreateDestinations < ActiveRecord::Migration[7.0]
+  def change
+    create_table :destinations do |t|
+      t.string :destination
+      t.string :meals
+      t.string :activities
+      t.integer :price
+
+      t.timestamps
+    end
+  end
+end
